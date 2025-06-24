c4abbd36264660de6078a6060d35216f9b4240c8
Artem <tema34468@gmail.com>
2025-06-24 13:19:05 +0300

    init

---
+++ learning-resources/src/app/learning-resources/learning-resources.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-resources',
+  imports: [],
+  templateUrl: './learning-resources.component.html',
+  styleUrl: './learning-resources.component.css'
+})
+export class LearningResourcesComponent {
+
+}
