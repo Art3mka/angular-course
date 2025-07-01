82dd57254a08d9ebd7fdcfe916026a339bbe9665
Artem <tema34468@gmail.com>
2025-07-01 13:14:58 +0300

    done ex

--- services-exercise/src/app/tasks/new-task/new-task.component.ts
+++ services-exercise/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,9 +10,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private readonly formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private readonly taskService: TasksService;
+
+  constructor(tService: TasksService) {
+    this.taskService = tService;
+  }
 
   onAddTask(title: string, description: string) {
+    this.taskService.addTask({ title, description });
     this.formEl()?.nativeElement.reset();
   }
 }
