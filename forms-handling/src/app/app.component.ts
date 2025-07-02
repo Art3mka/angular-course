3d6a79bec7c26efc797fdb9d377ca1e2c59ca73a
Artem <tema34468@gmail.com>
2025-07-02 13:43:36 +0300

    init

---
+++ forms-handling/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from './auth/login/login.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'forms-handling';
+}
